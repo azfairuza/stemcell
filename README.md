f0a942f8a0f327ec0fc403b24863f18e2f8610bc
azfairuza <fairuza.zacky1@gmail.com>
2022-04-04 23:20:33 +0700

    Adding method on Integrin class
    Adding getIntegrinDistance and getLigandDistance

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 ## UPDATE JOURNAL
 ### 20220404
-**1249**: starting writing the update journal, have been learning Linux, git, virtual environment, and zsh in the past week. 
+**1249**: start writing the update journal, have been learning Linux, git, virtual environment, and zsh in the past week. 
 
 **1720**: succesfully create cell object that contain integrin. My computer is so slow, need to be repaired as soon as possibe. 
 
 **2208**: start working again. Had unninstaled every windows program that seems heavy. Also configure windows settings to focus on performance. Result: improvement in performance.
+
+**2317**: Confused to find the best nearest neighbor algorithm. 
--- stemcell.py
+++ stemcell.py
@@ -1,15 +1,18 @@
 #Author: azfairuza
 #email: fairuza.zacky1@gmail.com
 
+from cmath import sqrt
 from random import uniform
-from math import sin
-from math import cos
 import numpy as np
 
 class Ligand():
     
-    def __init__(self):
-        pass
+    def __init__(self, x_position, y_position):
+        self.x_position = x_position
+        self.y_position = y_position
+        self.status = False
+        #False status mean that ligand is not connected with any integrin
+
 
 class Integrin():
     
@@ -18,18 +21,32 @@ class Integrin():
         self.radius = radius
         self.theta = theta
         self.integrin_id = integrin_id
+        self.status = False
     
     def getInformation(self):
         print(''.join(("integrin ", str(self.cell_id),".", 
             str(self.integrin_id), ": (", str(self.radius), ",", str(self.theta), ")")))
     
-    def getXYPosition(self, cell.x_center, cell.y_center):
+    def getXYPosition(self, cell):
         x_position = self.radius*np.cos(np.deg2rad(self.theta)) + cell.x_center
         y_position = self.radius*np.cos(np.deg2rad(self.theta)) + cell.y_center
         return(x_position, y_position)
     
+    def getLigandDistance(self, cell, target_ligand):
+        integrin_x_position, integrin_y_position = self.getXYPosition(cell)
+        x_distance = integrin_x_position - target_ligand.x_position
+        y_distance = integrin_y_position - target_ligand.y_position
+        return (sqrt(x_distance**2 + y_distance**2))
+    
+    def getIntegrinDistance(self, self_cell, target_integrin, target_cell):
+        self_x_position, self_y_position = self.getXYPosition(self_cell)
+        target_x_position, target_y_position = target_integrin.getXYPosition()
+        x_distance = self_x_position - target_x_position
+        y_distance = self_y_position - target_y_position
+        return(sqrt(x_distance**2 + y_distance**2))
 
 class Cell():
+    cell_number = 0
        
     def __init__(self, x_center, y_center, max_radius, total_integrin):
         self.x_center = x_center
@@ -46,7 +63,7 @@ class Cell():
         for reseptor in self.integrin:
             reseptor.getInformation()
     
-    cell_number = 0
+    
 
     
 
