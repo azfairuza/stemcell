413a03a1216d1c604e3d780936569cbab3e85cfb
azfairuza <fairuza.zacky1@gmail.com>
2022-05-27 16:44:02 +0700

    adding comments to the code

--- stemcell.py
+++ stemcell.py
@@ -7,81 +7,94 @@ from random import uniform
 import numpy as np
 
 class Ligand():
+# class for simulate ligand in nanopattern
 
-    ligand_number = 0
+    # Class properties
+    ligand_number = 0     #total ligand created
     
-    def __init__(self, x_position, y_position):
-        Ligand.ligand_number += 1
-        self.x_position = x_position
-        self.y_position = y_position
-        self.bound_status = False     # False status mean that ligand is not bounded to any integrin
-        self.targeted_status = False    # False status mean that ligand is not targeted by any integrin
-        self.ligand_id = Ligand.ligand_number
-        self.integrin_id = 0
+    def __init__(self, x_position: float, y_position: float):
+    # initial procedure when creating a ligand
+        
+        Ligand.ligand_number += 1                # Updating ligand number
+
+        # ligand properties
+        self.x_position = x_position             # ligand x-position
+        self.y_position = y_position             # ligand y-position
+        self.bound_status = False                # False: ligand is not bounded to any integrin
+        self.targeted_status = False             # False: ligand is not targeted by any integrin
+        self.ligand_id = Ligand.ligand_number    # id number for ligand
+        self.integrin_id = 0                     # id number for integrin connected to the ligand
         
 
 class Nanopattern():
+# class for simulate nanopattern
 
-    def __init__(self, height, width, grid_height, grid_width, position_list, dot_size):
+    def __init__(self, height: float, width: float, grid_height: float, grid_width: float, 
+        position_list: list , dot_size: float):
+    # inital procedure when creating a nanopattern
+        
         # nanopattern properties
-        self.height = height
-        self.width = width
-        self.grid_height = grid_height
-        self.grid_width = grid_width
-        self.position_seed = position_list
-        self.dot_size = dot_size
-        row_number = np.ceil(height/grid_height)
-        col_number = np.ceil(width/grid_width)
+        self.height = height                       # nanopattern height
+        self.width = width                         # nanopattern width
+        self.grid_height = grid_height             # nanopattern grid height
+        self.grid_width = grid_width               # nanopattern grid width
+        self.position_seed = position_list         # ligand position's list
+        self.dot_size = dot_size                   # ligand size (dot size in simulation)
+        row_number = np.ceil(height/grid_height)   # number of grid row created
+        col_number = np.ceil(width/grid_width)     # number of grid column created
 
         # nanopattern ligand members
         self.ligand = []
         for row in range(row_number):
             for col in range(col_number):
                 for position in position_list:
-                    self.ligand.append(Ligand(position[0] + col*grid_width, position[2] + row*grid_height))
+                    self.ligand.append(Ligand(position[0] + col*grid_width, position[1] + row*grid_height))
         
     
     
-    def getNotTargetedLigand(self):
+    def getNotTargetedLigand(self) -> list:
+    # Procedure to get a list of ligand that is not targeted by integrin
         members = []
         for ligand in self.ligand:
-            if ligand.bound_status == False:
+            if ligand.bound_status == False:       
                 members.append(ligand)
         return members
 
 
 class Integrin():
+# class to simulate integrin
 
+    # class properties
     number_integrin = 0
     
     def __init__(self, cell_id, x_center, y_center, max_radius):
-        # Update integrin number
-        Integrin.number_integrin += 1
+        
+        Integrin.number_integrin += 1         # Update integrin number
 
-        # identity
-        self.cell_id = cell_id
-        self.integrin_id = Integrin.number_integrin
+        # identity of integrin
+        self.cell_id = cell_id                       # cell id number
+        self.integrin_id = Integrin.number_integrin  # integrin id number 
         
         # position property
-        radius = round(uniform(0, max_radius), 2)
-        theta = round(uniform(0,360), 2)
-        self.x_position = radius*np.cos(np.deg2rad(theta)) + x_center
-        self.y_position = radius*np.cos(np.deg2rad(theta)) + y_center
+        radius = round(uniform(0, max_radius), 2)                      # distance from the cell's center
+        theta = round(uniform(0,360), 2)                               # the angle from x-axis
+        self.x_position = radius*np.cos(np.deg2rad(theta)) + x_center  # convert polar to x-position
+        self.y_position = radius*np.cos(np.deg2rad(theta)) + y_center  # convert polat to y-position
 
         # condition properties
         if radius >= 0.9*max_radius:
-            self.surface = True
+            self.surface = True        # definition: integrin is on the cell surface
         else:
-            self.surface = False
+            self.surface = False       # definition: integrin is in the inside of the cell
 
         # bound properties
-        self.bound_status = False
+        self.bound_status = False      # False: intgrin is not bounded to anyone
         self.object_type = 0           # 0: Empty ; 1: Ligand ; 2: Integrin
-        self.cell_target_id = 0
-        self.ligand_target_id = 0
-        self.integrin_target_id = 0
-        self.x_target = 0
-        self.y_target = 0
+        self.cell_target_id = 0        # cell id number for integrin-integrin complex
+        self.ligand_target_id = 0      # ligand id number for integrin-ligand complex 
+        self.integrin_target_id = 0    # integrin id number for integrin-integrin complex
+        self.x_target = 0              # x-position of the targeted object
+        self.y_target = 0              # y-position of the 
     
     def getInformation(self):
         if self.bound_status == True:
